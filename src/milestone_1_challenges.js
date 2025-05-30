cb69e842dae09a54dbd44385433e5d6fe976f55c
Muchiri-cmd <davismuchiri21@gmail.com>
2025-05-30 15:08:33 +0300

    Complete challenge1 : Sum of 2 Numbers

---
+++ src/milestone_1_challenges.js
@@ -0,0 +1,7 @@
+//Challenge 1: Return th sum of Two Numbers
+function addition(num1, num2) {
+  return num1 + num2;
+}
+//Test cases
+console.log(addition(3, 5)); //8
+console.log(addition(-6, 9)); // 3)
---
+++ src/milestone_1_challenges.ts
@@ -0,0 +1,8 @@
+//Challenge 1: Return th sum of Two Numbers
+function addition(num1: number, num2: number): number {
+  return num1 + num2;
+}
+
+//Test cases
+console.log(addition(3, 5)); //8
+console.log(addition(-6, 9)); // 3)
