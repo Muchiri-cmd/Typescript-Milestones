d0bd2969d8d97f15a13b5f77cd0b1d3c47e75d35
Muchiri-cmd <davismuchiri21@gmail.com>
2025-05-31 09:50:22 +0300

    Complete challenge2: Find Maximum Value

--- src/milestone_2_challenges.js
+++ src/milestone_2_challenges.js
@@ -8,6 +8,18 @@ function sumOfPositives(array) {
     }
     return sum;
 }
+//Challenge 2: Find Max value
+function findMax(array) {
+    var maxNumber = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > maxNumber) {
+            maxNumber = array[i];
+        }
+    }
+    return maxNumber;
+}
 //Test cases
 console.log("Sum of Positives");
-console.log(sumOfPositives([1, -3, 5, -2, 9, -8])); // 15)
+console.log(sumOfPositives([1, -3, 5, -2, 9, -8])); // 15
+console.log("Find Maximum Value");
+console.log(findMax([3, 7, 2, 9, 5])); // returns: 9
--- src/milestone_2_challenges.ts
+++ src/milestone_2_challenges.ts
@@ -9,6 +9,20 @@ function sumOfPositives(array: number[]): number {
   return sum;
 }
 
+//Challenge 2: Find Max value
+function findMax(array: number[]): number {
+  let maxNumber = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maxNumber) {
+      maxNumber = array[i];
+    }
+  }
+  return maxNumber;
+}
+
 //Test cases
 console.log("Sum of Positives");
-console.log(sumOfPositives([1, -3, 5, -2, 9, -8])); // 15)
+console.log(sumOfPositives([1, -3, 5, -2, 9, -8])); // 15
+
+console.log("Find Maximum Value");
+console.log(findMax([3, 7, 2, 9, 5])); // returns: 9
